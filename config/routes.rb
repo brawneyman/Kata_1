82f8b9fb7a7645517cce62811f94f5b99ff0698e
Braun Bailey <braun.bailey@octanner.com>
2013-06-25 13:20:58 -0600

    fix_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Kata::Application.routes.draw do
+  resources :posts
+
   root :to=> "home#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
